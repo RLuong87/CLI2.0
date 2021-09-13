c11c003816734a6af6eeed9c4aec7c25805cc41e
RLuong87 <richiesluong@gmail.com>
2021-09-13 12:21:02 -0400

    rock, papers, scissors edabit challenge

--- src/com/company/RockPaperScissors.java
+++ src/com/company/RockPaperScissors.java
@@ -4,36 +4,35 @@ import java.util.Random;
 
 public class RockPaperScissors {
 
-//    Create a function which simulates the game "rock, paper, scissors".
-//    The function takes the input of both players (rock, paper or scissors),
-//    first parameter from first player, second from second player.
-//    The function returns the result as such:
-//
-//            "Player 1 wins"
-//            "Player 2 wins"
-//            "TIE" (if both inputs are the same)
-//    The rules of rock, paper, scissors, if not known:
-//
-//    Both players have to say either "rock", "paper" or "scissors" at the same time.
-//    Rock beats scissors, paper beats rock, scissors beat paper
-
-
     public static String rps(String s1, String s2) {
 
-//        String[] rpsArr = { "rock", "paper", "scissors"};
-//        Random r = new Random();
-//        int rand = r.nextInt(rpsArr.length);
+        String rock = new String("rock");
+        String paper = new String("paper");
+        String scissors = new String("scissors");
 
-        return "";
+        if (s1.equals(paper) && s2.equals(rock) || s1.equals(rock) && s2.equals(scissors) || s1.equals(scissors) && s2.equals(paper)) {
+            return "Player 1 wins";
+        } else if (s2.equals(paper) && s1.equals(rock) || s2.equals(rock) && s1.equals(scissors) || s2.equals(scissors) && s1.equals(paper)) {
+            return "Player 2 wins";
+        } else {
+            return "TIE";
+        }
     }
 
     public static void main(String[] args) {
-//        System.out.println(rps("", ""));
-
-        String[] rpsArr = { "rock", "paper", "scissors" };
-        Random r = new Random();
-        int rand = r.nextInt(rpsArr.length);
-        System.out.println(rpsArr[rand]);
-
+        System.out.println(rps("rock", "scissors"));
+        System.out.println(rps("paper", "rock"));
+        System.out.println(rps("scissors", "paper"));
+        System.out.println(rps("rock", "paper"));
+        System.out.println(rps("scissors", "rock"));
+        System.out.println(rps("paper", "scissors"));
+        System.out.println(rps("rock", "rock"));
+        System.out.println(rps("scissors", "scissors"));
+        System.out.println(rps("paper", "paper"));
     }
 }
+
+//        String[] rpsArr = { "rock", "paper", "scissors" };
+//        Random r = new Random();
+//        int rand = r.nextInt(rpsArr.length);
+//        System.out.println(rpsArr[rand]);
